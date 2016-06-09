dc05fcc99caf56324b696ae838d1b8936e521074
Raul Alzate <raul.alzate@techandsolve.com>
2016-06-09 16:40:12 -0500

    se realiza el dump de la base de datos

--- dump.sql
+++ dump.sql
@@ -1,78 +1,109 @@
--- MySQL Workbench Forward Engineering
+-- MySQL dump 10.13  Distrib 5.7.12, for Linux (x86_64)
+--
+-- Host: localhost    Database: cards
+-- ------------------------------------------------------
+-- Server version	5.7.12-0ubuntu1
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Schema cards
--- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `cards` ;
+--
+-- Table structure for table `bill`
+--
 
--- -----------------------------------------------------
--- Schema cards
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `cards` DEFAULT CHARACTER SET latin1 ;
-USE `cards` ;
+DROP TABLE IF EXISTS `bill`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `bill` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `label` varchar(45) NOT NULL,
+  `description` varchar(45) NOT NULL,
+  `mount` double NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `cards`.`client`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `cards`.`client` ;
+--
+-- Dumping data for table `bill`
+--
 
-CREATE TABLE IF NOT EXISTS `cards`.`client` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `cedula` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+LOCK TABLES `bill` WRITE;
+/*!40000 ALTER TABLE `bill` DISABLE KEYS */;
+INSERT INTO `bill` VALUES (1,'factura','Hola factura',500000),(2,'mi factura','descripcion',123123123),(3,'Mi factura','asdasd',123123),(8,'otra factura','asdasd',111111);
+/*!40000 ALTER TABLE `bill` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `card`
+--
 
--- -----------------------------------------------------
--- Table `cards`.`card`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `cards`.`card` ;
-
-CREATE TABLE IF NOT EXISTS `cards`.`card` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `label` VARCHAR(45) NOT NULL,
-  `status` TINYINT(1) NOT NULL DEFAULT '1',
-  `amount` DOUBLE NOT NULL DEFAULT '0',
-  `bonus` DOUBLE NULL,
-  `cut_date` DATETIME NULL DEFAULT now(),
-  `client_id` INT NOT NULL,
+DROP TABLE IF EXISTS `card`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `card` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `label` varchar(45) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  `amount` double NOT NULL DEFAULT '0',
+  `bonus` double DEFAULT NULL,
+  `cut_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `client_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_card_client_idx` (`client_id` ASC),
-  CONSTRAINT `fk_card_client`
-    FOREIGN KEY (`client_id`)
-    REFERENCES `cards`.`client` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+  KEY `fk_card_client_idx` (`client_id`),
+  CONSTRAINT `fk_card_client` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=64 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `card`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+LOCK TABLES `card` WRITE;
+/*!40000 ALTER TABLE `card` DISABLE KEYS */;
+INSERT INTO `card` VALUES (1,'Tarjeta Raul',0,200000,0,NULL,3),(2,'Tarjeta 2 Raul',0,300000,0,NULL,3);
+/*!40000 ALTER TABLE `card` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- -----------------------------------------------------
--- Data for table `cards`.`client`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `cards`;
-INSERT INTO `cards`.`client` (`id`, `name`, `cedula`) VALUES (3, 'Raul Andres Alzate', '1115069076');
+--
+-- Table structure for table `client`
+--
 
-COMMIT;
+DROP TABLE IF EXISTS `client`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `client` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `cedula` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `client`
+--
 
--- -----------------------------------------------------
--- Data for table `cards`.`card`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `cards`;
-INSERT INTO `cards`.`card` (`id`, `label`, `status`, `amount`, `bonus`, `cut_date`, `client_id`) VALUES (1, 'Tarjeta Raul', 0, 200000, 0, NULL, 3);
-INSERT INTO `cards`.`card` (`id`, `label`, `status`, `amount`, `bonus`, `cut_date`, `client_id`) VALUES (2, 'Tarjeta 2 Raul', 0, 300000, 0, NULL, 3);
+LOCK TABLES `client` WRITE;
+/*!40000 ALTER TABLE `client` DISABLE KEYS */;
+INSERT INTO `client` VALUES (3,'Raul Andres Alzate','1115069076');
+/*!40000 ALTER TABLE `client` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-COMMIT;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
+-- Dump completed on 2016-06-09 16:39:12
